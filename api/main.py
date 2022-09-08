951e8499325a1d097cc89d1860d5d59c229c868c
iisyos <kosuke1216a@yahoo.co.jp>
2022-09-08 21:01:47 +0900

    モデルの作成

--- api/main.py
+++ api/main.py
@@ -1,3 +1,27 @@
+from pydantic import BaseModel
 from fastapi import FastAPI
+from machineLearning.titanic import PredictOnAPI
 
 app = FastAPI()
+
+
+class SchemaOfTitanicFeaturesRequest(BaseModel):
+    Sex: str
+    Pclass: str
+    Age: int
+    Parch: int
+    SibSp: int
+
+
+class SchemaOfSurvivalProbabilityResponse(BaseModel):
+    survival_probability: float
+
+
+@app.post('/api/titanic', response_model=SchemaOfSurvivalProbabilityResponse)
+def derive_score(request_body: SchemaOfTitanicFeaturesRequest):
+    # 辞書形式に変更
+    features_dict = request_body.__dict__
+    # **<辞書オブジェクト>とすることで引数として自動的にバラして与えることが可能
+    survival_probability = PredictOnAPI.derive_survival_probability(
+        **features_dict)
+    return {'survival_probability': survival_probability}
